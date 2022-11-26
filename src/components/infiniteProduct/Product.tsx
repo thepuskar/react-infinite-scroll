0d6896b1f92d745cc6d46a81e733a40334b3aa36
Puskar Adhikari <puskar.ad@gmail.com>
2022-11-26 23:54:47 +0545

    enable initial load and stay render data

--- src/components/infiniteProduct/Product.tsx
+++ src/components/infiniteProduct/Product.tsx
@@ -21,7 +21,7 @@ export const Product = () => {
           itemContent={(index, data) => {
             return (
               <div>
-                <RenderIfVisible>
+                <RenderIfVisible initialVisible={true} stayRendered={true}>
                   <div className='m-4 flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
                     <img
                       className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
